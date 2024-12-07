f9363d9110445157b6af41f8beacbce6e1e3e0ad
Tymek <tymo@student.agh.edu.pl>
2024-12-07 19:23:44 +0100

    first commit/startup page

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,23 @@
 
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='../frontend/templates')
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/reflected_xss')
+def reflected_xss():
+    return render_template('reflected_xss.html')
+
+@app.route('/stored_xss')
+def stored_xss():
+    return render_template('stored_xss.html')
+
+@app.route('/dom_xss')
+def dom_xss():
+    return render_template('dom_xss.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
