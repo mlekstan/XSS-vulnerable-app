f50084f7e3aa26bb62b24de411b89ed3b5cbab1b
Tymek <tymo@student.agh.edu.pl>
2024-12-08 15:31:13 +0100

    startup page and editing tasks page

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__, template_folder='../frontend/templates', static_folder='..
 def home():
     return render_template('index.html')
 
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html')
+
 @app.route('/reflected_xss')
 def reflected_xss():
     return render_template('reflected_xss.html')
@@ -18,6 +22,7 @@ def stored_xss():
 @app.route('/dom_xss')
 def dom_xss():
     return render_template('dom_xss.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,3 +1,1 @@
-document.getElementById("clickBtn").addEventListener("click", function() {
-    document.getElementById("message").textContent = "Dziękujemy za kliknięcie!";
-});
+
